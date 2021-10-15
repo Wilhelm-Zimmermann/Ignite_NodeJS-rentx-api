c770e6a565bbb1d490a3ea93eaa99ae062e2a407
Wilhelm <wilhelmzimmermann8@gmail.com>
2021-10-15 16:13:46 -0300

    Babel

--- babel.config.js
+++ babel.config.js
@@ -8,10 +8,11 @@ module.exports = {
             "module-resolver",
             {
                 alias: {
-                    "@modules": ["./src/modules"],
-                    "@config": ["./src/config"],
-                    "@shared": ["./src/shared"],
-                    "@errors": ["./src/errors"],
+                    "@modules": "./src/modules",
+                    "@config": "./src/config",
+                    "@shared": "./src/shared",
+                    "@errors": "./src/errors",
+                    "@utils":"./src/utils"
                 }
             }
         ],
